3627eed02d6be5c29ed6bf8421e8778618c94380
caffeinated-squirrel <caffeinated_squirrel@nyppo.com>
2017-05-25 16:00:39 -0400

    Health Bars

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float maxHealthPoints = 100f;
+
+    float currentHealthPoints = 100f;
+
+    public float healthAsPercentage {
+        get {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
