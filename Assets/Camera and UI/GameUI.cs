0434c83003afb2f02078c13fc178840ef89d0ca0
caffeinated-squirrel <caffeinated_squirrel@nyppo.com>
2017-05-29 16:17:21 -0400

    Village1 and Minimap

---
+++ Assets/Camera and UI/GameUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponentInChildren<Text>().text = SceneManager.GetActiveScene().name;
+	}
+}
---
+++ Assets/Camera and UI/MiniMap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour {
+
+    private void OnPreCull() {
+        GetComponentInChildren<Light>().enabled = true;
+    }
+
+    private void OnPostRender() {
+        GetComponentInChildren<Light>().enabled = false;
+    }
+}
