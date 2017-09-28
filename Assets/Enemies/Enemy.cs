2c67fccde6f38db9d67152b5abf3d3c5a744119b
arumenn <abel.laflamme@gmail.com>
2017-09-28 14:43:51 -0400

    Enemies chasing

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 4f;
 
     float currentHealthPoints = 100f;
+    AICharacterControl aICharacterControl = null;
+    GameObject player = null;
 
     public float healthAsPercentage {
         get {
@@ -17,11 +21,18 @@ public class Enemy : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-
+        player = GameObject.FindGameObjectWithTag("Player");
+        aICharacterControl = GetComponent<AICharacterControl>();
     }
 
     // Update is called once per frame
     void Update() {
-
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= attackRadius) {
+            //start chasing
+            aICharacterControl.SetTarget(player.transform);
+        } else {
+            aICharacterControl.SetTarget(transform);
+        }
     }
 }
