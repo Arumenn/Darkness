16fd6eb8c00ec0e5282b16cc866d6c6bfb744cf0
arumenn <abel.laflamme@gmail.com>
2017-10-02 12:10:55 -0400

    Projectile lifespan

--- Assets/Enemies/Projectile.cs
+++ Assets/Enemies/Projectile.cs
@@ -7,8 +7,21 @@ public class Projectile : MonoBehaviour {
     public float projectileSpeed = 10f;
     private float damage = 5f;
 
+    //projectile lifetime
+    private float timeToLive = 5f;
+    private float timeBorn = 0f;
+
+    private void LateUpdate() {
+        if (Time.time - timeBorn >= timeToLive) {
+            print("dead projectile");
+            Destroy(gameObject);
+        }
+    }
+
     public void SetDamage(float d) {
         damage = d;
+        timeBorn = Time.time;
+        print("new projectile");
     }
 
     private void OnTriggerEnter(Collider collider) {
